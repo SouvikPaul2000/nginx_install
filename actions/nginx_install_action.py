ea792b3749b92c9889076ca4bf0539f0bf7f86b7
Souvik <shazra@zuelligpharma.com>
2023-06-15 12:46:37 +0530

    custom pack for nginx install

---
+++ actions/nginx_install_action.py
@@ -0,0 +1,20 @@
+import paramiko
+
+from st2common.runners.base_action import Action
+
+class Nginx(Action):
+    def run(self):
+
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(hostname='0.0.0.0', port='8080')
+
+        # Install Nginx
+        stdin, stdout, stderr = ssh_client.exec_command('apt-get update')
+        print(stdout.read().decode())
+
+        stdin, stdout, stderr = ssh_client.exec_command('apt-get install -y nginx')
+        print(stdout.read().decode())
+
+        ssh_client.close()
+
